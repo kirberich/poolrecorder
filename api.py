522ebd10a3d514b5b919706799273d732b4c420d
Robert Kirberich <robert@kirberich.de>
2013-08-05 22:31:51 +0100

    Rejig entire api to allow super-simple streaming

--- api.py
+++ api.py
@@ -1,57 +1,134 @@
-from twisted.web import server, resource
-from twisted.web.resource import NoResource
-from twisted.internet import reactor
+from twisted.web import http
+from twisted.web.http import HTTPChannel
+from twisted.internet import reactor, defer
 import threading
 import os
 
-# Resource definitions
-class RecorderResource(object, resource.Resource):
-	isLeaf = True
+
+class RecorderHandler(http.Request, object):
+	BOUNDARY = "jpgboundary"
 
 	def __init__(self, api, *args, **kwargs):
-		self.api = api 
-		super(RecorderResource, self).__init__(*args, **kwargs)
+		self.api = api
+		super(RecorderHandler, self).__init__(*args, **kwargs)
 
-	def serve_jpg(self, request, jpg_frame):
-		request.setHeader("content-type", "image/jpg")
-		return jpg_frame.tostring()
+	def get_frame(self):
+		return self.api.recorder.current_jpg_frame
 
-	def serve_latest_video(self, request):
+	def wait(self, seconds, result=None):
+		"""Returns a deferred that will be fired later"""
+		d = defer.Deferred()
+		reactor.callLater(seconds, d.callback, result)
+		return d
+
+	def writeBoundary(self):
+		self.write("--%s\n" % (self.BOUNDARY))
+
+	def writeStop(self):
+		self.write("--%s--\n" % (self.BOUNDARY))
+
+	def not_found(self, message=None):
+		self.setResponseCode(404, message)
+		self.finish()
+
+	def simple_render(self, content, content_type="text/plain"):
+		self.render(content, [("Content-Type", content_type)])
+
+	def render(self, content, headers):
+		for (header_name, header_value) in headers:
+			self.setHeader(header_name, header_value)
+		self.write(content)
+		self.finish()
+
+	@defer.inlineCallbacks
+	def serve_stream(self):
+		""" Serve video stream as multi-part jpg. Yes, this actually works. """
+		boundary = "jpgboundary"
+
+		self.setHeader('Connection', 'Keep-Alive')
+		self.setHeader('Content-Type', "multipart/x-mixed-replace;boundary=%s" % boundary)
+
+		while True:
+			content = self.get_frame()
+			self.write("Content-Type: image/jpg\n")
+			self.write("Content-Length: %s\n\n" % (len(content)))
+			self.write(content)
+			self.write("--%s\n" % (boundary))
+			yield self.wait(0.05)
+
+	def serve_frame(self):
+		return self.simple_render(self.get_frame(), "image/jpg")
+
+	def serve_latest_video(self):
+
 		candidates = os.listdir(".")
 		candidates = sorted([x for x in candidates if x.startswith("pool-")])
 		if not candidates:
-			return NoResource
-
+			return self.not_found()
 
 		file_name = candidates[0]
 		f = open(file_name)
 		content = f.read()
 
-		request.setHeader("content-type", "application/octet-stream")
-		request.setHeader('content-disposition', 'attachment; filename="%s"' % file_name)
-		return content
+		headers = [
+			("content-type", "application/octet-stream"),
+			('content-disposition', 'attachment; filename="%s"' % file_name)
+		]
+		self.render(content, headers)
 
-	def render_GET(self, request):
-		path = request.path[1:]
-		if path == "save":
-			self.api.trigger("save")
-		elif path == "quit":
-			self.api.trigger("quit")
-		elif path == "current" or path == "current.jpg":
-			return self.serve_jpg(request, self.api.recorder.current_jpg_frame)
-		elif path == "latest_video" or path == "latest_video.avi":
-			return self.serve_latest_video(request)
+	def process(self):
+		command_args_list = [x for x in self.path.split("/") if x]
+		command = ""
+		args = []
+		if command_args_list:
+			command = command_args_list[0]
+			args = command_args_list[1:]
 
-		return "'ello."
+		try:
+			if command == "save":
+				self.api.trigger("save")
+				return self.simple_render("Saving video.")
+			elif command == "quit":
+				self.api.trigger("quit")
+				return self.simple_render("Quitting.")
+			elif command.startswith("current"):
+				return self.serve_frame()
+			elif command.startswith("latest_video"):
+				return self.serve_latest_video()
+			elif command.startswith("stream"):
+				return self.serve_stream()
+			elif command == "echo":
+				return self.simple_render(args[0])
+		except Exception, e:
+			return self.simple_render(e.message)
+
+		return self.simple_render("Invalid command: %s" % command)
+
+
+class RecorderHandlerFactory(object):
+	def __init__(self, api):
+		self.api = api 
+
+	def __call__(self, *args, **kwargs):
+		return RecorderHandler(self.api, *args, **kwargs)
+
+
+class StreamFactory(http.HTTPFactory):
+	protocol = HTTPChannel
+
 
 class Api:
 	""" An api for a pool recorder, uses a twisted server inside a thread to keep track of webby things. """
 
 	def __init__(self, recorder):
+		# This I believe is what you find when you look up "ugly" in the dictionary
+		# But I really don't want to try and understand this FactoryFactoryFactory stuff properly
+		HTTPChannel.requestFactory = RecorderHandlerFactory(api=self)
+
 		self.recorder = recorder
 		self.events = []
 
-		reactor.listenTCP(8080, server.Site(RecorderResource(self)))
+		reactor.listenTCP(8080, StreamFactory())
 		t = threading.Thread(target=reactor.run)
 		t.daemon = True
 		t.start()
